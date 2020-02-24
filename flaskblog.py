01fb6389672c6b812c106eb0443a4108545c07fa
Ahmed Hasan <hasan.ahmedmonjurul@usask.ca>
2020-02-23 18:30:50 -0600

    requirements.txt added

--- flaskblog.py
+++ flaskblog.py
@@ -1,18 +1,19 @@
 from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ama_cloud.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ama_cloud1.db'
 db = SQLAlchemy(app)
 
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False)
     content = db.Column(db.Text, nullable=False)
     author = db.Column(db.Text, nullable=False)
     udacity = db.Column(db.Text, nullable=False)
@@ -26,7 +27,7 @@ class Post(db.Model):
 @app.route("/home")
 def home():
     page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=3)
+    posts = Post.query.order_by(Post.date_posted.asc()).paginate(page=page, per_page=3)
     return render_template('home.html', posts=posts)
 
 
@@ -60,10 +61,14 @@ def user_posts(username):
     page = request.args.get('page', 1, type=int)
     #user = User.query.filter_by(username=username).first_or_404()
     posts = Post.query.filter_by(author=username)\
-        .order_by(Post.date_posted.desc())\
-        .paginate(page=page, per_page=3)
+        .order_by(Post.date_posted.asc())
     return render_template('user_posts.html', posts=posts, user=username)
 
+@app.route("/graph1")
+def graph1():
+    #graph1=Post.query(Post.date_posted, func.count(Post.date_posted)).group_by(Post.date_posted).all()
+    posts = Post.query.order_by(Post.date_posted.asc())
+    return render_template('graph1.html', graph1=posts)
 
 if __name__ == '__main__':
     app.run(debug=True)
